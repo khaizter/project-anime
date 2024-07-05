76d2a9f8358f9a6793a96646b7bdeb8528962ac7
khaizter <vashh.dev@gmail.com>
2024-07-05 22:57:11 +0800

    Added profile page

---
+++ pages/profile/index.tsx
@@ -0,0 +1,19 @@
+import Wrapper from "@/components/wrapper";
+import React from "react";
+
+const ProfilePage = () => {
+  return (
+    <Wrapper>
+      <div className="flex">
+        <div>Username:</div>
+        <div>USERNAME</div>
+      </div>
+      <div>
+        <div>My Favorites</div>
+        <div>SHOW GRID OF FAVORITES</div>
+      </div>
+    </Wrapper>
+  );
+};
+
+export default ProfilePage;
