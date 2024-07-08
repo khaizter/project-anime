49fa74023393a5fdaacd3d0c5e675a67e4cccd84
khaizter <vashh.dev@gmail.com>
2024-07-09 06:31:13 +0800

    favorite get api

--- pages/api/user/favorite.ts
+++ pages/api/user/favorite.ts
@@ -3,15 +3,9 @@ import { authOptions } from "@/pages/api/auth/[...nextauth]";
 import { NextApiRequest, NextApiResponse } from "next";
 import { getServerSession } from "next-auth";
 
-const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  if (req.method !== "PATCH") {
-    return res.status(400).json({ message: "Invalid method!" });
-  }
-
+const methodPatch = async (req: NextApiRequest, res: NextApiResponse) => {
   const { body } = req;
   const { animeId, remove } = body;
-  console.log(animeId, remove);
-
   // get session
   const session = await getServerSession(req, res, authOptions);
 
@@ -52,6 +46,47 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   // send 200 code
   client.close();
   return res.status(200).json({ message: "Favorites updated!" });
+};
+
+const methodGet = async (req: NextApiRequest, res: NextApiResponse) => {
+  // get session
+  const session = await getServerSession(req, res, authOptions);
+
+  if (!session) {
+    return res.status(401).json({ message: "Not authenticated!" });
+  }
+
+  // database connect
+  const client = await connectToDatabase();
+  const db = client.db();
+
+  // check for user
+  const user = await db
+    .collection("users")
+    .findOne({ username: session?.user?.name });
+  if (!user) {
+    client.close();
+    return res.status(404).json({ message: "No user found!" });
+  }
+
+  var favorites = user.favorites || [];
+
+  client.close();
+  return res
+    .status(200)
+    .json({ message: "Fetched successfully!", data: favorites });
+};
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method === "PATCH") {
+    return methodPatch(req, res);
+  } else if (req.method === "GET") {
+    return methodGet(req, res);
+  } else {
+    return res.status(400).json({ message: "Invalid method!" });
+  }
+
+  methodPatch(req, res);
 };
 
 export default handler;
