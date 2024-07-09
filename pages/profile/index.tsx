aae3b7579f6ff51daf7d63064dc18c8527ee8c37
Khaizter Vash Topacio <vashh.dev@gmail.com>
2024-07-10 02:54:44 +0800

    show favorites anime id on profile

--- pages/profile/index.tsx
+++ pages/profile/index.tsx
@@ -2,13 +2,39 @@ import Wrapper from "@/components/wrapper";
 import { GetServerSideProps } from "next";
 import { useSession } from "next-auth/react";
 import Link from "next/link";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { authOptions } from "../api/auth/[...nextauth]";
 import { getServerSession } from "next-auth";
+import { Button } from "@/components/ui/button";
 
 const ProfilePage = () => {
   const { data: session, status } = useSession();
-  console.log(session, status);
+  const [favorites, setFavorites] = useState<Array<number>>([]);
+
+  useEffect(() => {
+    const fetchFavorites = async () => {
+      const response = await fetch("/api/user/favorite", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      const result = await response.json();
+
+      if (!response.ok) {
+        throw new Error(result.message || "Something went wrong!");
+      }
+
+      setFavorites(result.data);
+    };
+    fetchFavorites();
+  }, []);
+
+  const unfavoriteHandler = (animeId: number) => {
+    console.log("unfavorite: ", animeId);
+  };
+
   return (
     <Wrapper>
       <div className="flex">
@@ -19,6 +45,13 @@ const ProfilePage = () => {
       <div>
         <div>My Favorites</div>
         <div>SHOW GRID OF FAVORITES</div>
+        {favorites.map((item: number) => {
+          return (
+            <Button key={item} onClick={unfavoriteHandler.bind(null, item)}>
+              {item}
+            </Button>
+          );
+        })}
       </div>
     </Wrapper>
   );
@@ -26,8 +59,6 @@ const ProfilePage = () => {
 
 export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
   const session = await getServerSession(req, res, authOptions);
-
-  console.log("MY SESSIOn", session);
 
   if (!session) {
     return {
