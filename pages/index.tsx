a87076e32f0302e5886c11fc1f71603fd2f28055
khaizter <vashh.dev@gmail.com>
2024-07-12 13:06:20 +0800

    Index page search bar

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,38 @@
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import Wrapper from "@/components/wrapper";
 import Image from "next/image";
 import Link from "next/link";
+import { useRouter } from "next/router";
+import { useRef } from "react";
 
 export default function Home() {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const router = useRouter();
+
+  const searchHandler = () => {
+    if (!inputRef || !inputRef.current) {
+      throw Error("no input ref");
+    }
+    router.push({
+      pathname: "/filter",
+      query: {
+        keyword: inputRef.current.value,
+      },
+    });
+  };
+
   return (
-    <main>
-      <div>SEARCH BAR</div>
+    <Wrapper>
+      <div className="flex items-center">
+        <Input type="text" ref={inputRef} />
+        <Button type="button" onClick={searchHandler}>
+          Search
+        </Button>
+      </div>
       <Link href="/home">
         <Button>Go to Home</Button>
       </Link>
-    </main>
+    </Wrapper>
   );
 }
