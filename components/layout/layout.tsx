0733f5d9c4063c7fb37511570ca1f9b17390889a
khaizter <vashh.dev@gmail.com>
2024-07-13 06:22:19 +0800

    menu links

--- components/layout/layout.tsx
+++ components/layout/layout.tsx
@@ -1,13 +1,16 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useEffect } from "react";
 import MainHeader from "@/components/layout/main-header";
 
 import { spaceGrotesk, rajdhani, majorMonoDisplay } from "@/lib/fonts";
 
 const Layout = (props: any) => {
+  useEffect(() => {
+    console.log("layout");
+  }, []);
   return (
     <div className={`${spaceGrotesk} ${rajdhani} ${majorMonoDisplay}`}>
       <MainHeader />
-      <main className="font-rajdhani text-slate-100 bg-gradient-to-b from-kingfisher-daisy to-black">
+      <main className="font-rajdhani text-slate-100 bg-gradient-to-b from-jacaranda to-black">
         {props.children}
       </main>
     </div>
--- components/layout/main-header.tsx
+++ components/layout/main-header.tsx
@@ -4,15 +4,34 @@ import Wrapper from "@/components/wrapper";
 import { useSession, signOut } from "next-auth/react";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import React, { useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Menu, Search } from "lucide-react";
+import {
+  Sheet,
+  SheetContent,
+  SheetTrigger,
+  SheetClose,
+} from "@/components/ui/sheet";
+import { getGenres } from "@/lib/utils";
 
 const MainHeader = () => {
   const inputRef = useRef<HTMLInputElement>(null);
   const router = useRouter();
+  const [genres, setGenres] = useState<Array<string>>([]);
   const { data: session, status } = useSession();
 
-  console.log(router.pathname);
+  useEffect(() => {
+    const fetchGenres = async () => {
+      try {
+        const genres = await getGenres();
+        console.log(genres);
+        setGenres(genres);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchGenres();
+  }, []);
 
   const searchHandler = () => {
     if (!inputRef || !inputRef.current) {
@@ -35,9 +54,59 @@ const MainHeader = () => {
     <header className=" bg-kingfisher-daisy text-slate-100">
       <Wrapper className="py-4 flex items-center justify-between">
         <div className="flex items-center space-x-6">
-          <Button variant="ghost" size="icon">
-            <Menu className="h-8 w-8" />
-          </Button>
+          <Sheet>
+            <SheetTrigger asChild>
+              <Button variant="ghost" size="icon">
+                <Menu className="h-8 w-8" />
+              </Button>
+            </SheetTrigger>
+            <SheetContent
+              className="flex flex-col bg-kingfisher-daisy text-white border-none pt-16 px-0 gap-0"
+              side="left"
+            >
+              <ul className="font-space-grotesk text-2xl text-white">
+                <li className="py-3 px-3 border-b-2 border-kingfisher-daisy-800/25">
+                  <SheetClose asChild>
+                    <Link href={"/home"}>Home</Link>
+                  </SheetClose>
+                </li>
+                <li className="py-3 px-3 border-b-2 border-kingfisher-daisy-800/25">
+                  <SheetClose asChild>
+                    <Link href={"/popular"}>Popular</Link>
+                  </SheetClose>
+                </li>
+                <li className="py-3 px-3 border-b-2 border-kingfisher-daisy-800/25">
+                  <SheetClose asChild>
+                    <Link href={"/trending"}>Trending</Link>
+                  </SheetClose>
+                </li>
+              </ul>
+              <div className="font-space-grotesk text-2xl p-3 text-lavender-magenta">
+                Genre
+              </div>
+              <ul className="grid grid-cols-2 gap-2 text-xl p-3">
+                {genres.map((item: string, index: number) => {
+                  return (
+                    <li key={index}>
+                      <SheetClose asChild>
+                        <Link
+                          href={{
+                            pathname: "/filter",
+                            query: {
+                              genres: [item],
+                            },
+                          }}
+                        >
+                          {item}
+                        </Link>
+                      </SheetClose>
+                    </li>
+                  );
+                })}
+              </ul>
+            </SheetContent>
+          </Sheet>
+
           <Link className="font-major-mono-display text-3xl" href={"/home"}>
             Project<span className="text-lavender-rose">Anime</span>
           </Link>
@@ -52,7 +121,7 @@ const MainHeader = () => {
                 className="bg-transparent py-2 px-4 font-space-grotesk"
                 onClick={searchHandler}
               >
-                <Search className="h-4 w-4" color="#340181" />
+                <Search className="h-4 w-4" color="#3B0086" />
               </button>
               <Button size="sm">Filter</Button>
             </div>
