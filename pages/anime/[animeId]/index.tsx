444256aa8f8cc4a034abbc72c046112776bc5097
khaizter <vashh.dev@gmail.com>
2024-07-22 13:00:02 +0800

    Anime Sidebar

--- pages/anime/[animeId]/index.tsx
+++ pages/anime/[animeId]/index.tsx
@@ -9,6 +9,7 @@ import { Heart } from "lucide-react";
 import { useSession } from "next-auth/react";
 import { AnimeType } from "@/lib/types";
 import { ToggleGroup, ToggleGroupItem } from "@/components/ui/toggle-group";
+import Link from "next/link";
 
 const addFavorite = async (
   animeId: string | number,
@@ -114,99 +115,135 @@ const AnimeDetailPage = (props: any) => {
       </div>
       <Wrapper>
         <div className="flex items-start">
-          <div className="relative m-4 min-w-52">
-            <Image
-              src={coverImage.large || ""}
-              alt={`${title.romaji} cover image`}
-              width={0}
-              height={0}
-              sizes="100vw"
-              className="w-full h-auto"
-            />
-            <Button
-              onClick={favoriteHandler}
-              disabled={isLoading || status === "loading"}
-            >
-              <Heart
-                className="mr-2 h-4 w-4"
-                color={isFavorite ? "#ff0000" : "#ff0000"}
-                fill={isFavorite ? "#ff0000" : "transparent"}
+          <div className="relative m-4 min-w-52 space-y-6">
+            <div className="space-y-4">
+              <Image
+                src={coverImage.large || ""}
+                alt={`${title.romaji} cover image`}
+                width={0}
+                height={0}
+                sizes="100vw"
+                className="w-full h-auto"
               />
-              Add to Favorites
-            </Button>
-            <div>
-              <span>Episodes:</span>
-              <span className="text-white/60">{episodes}</span>
+              <Button
+                className="w-full"
+                onClick={favoriteHandler}
+                disabled={isLoading || status === "loading"}
+              >
+                <Heart
+                  className="mr-2 h-4 w-4"
+                  color={isFavorite ? "#ff0000" : "#ff0000"}
+                  fill={isFavorite ? "#ff0000" : "transparent"}
+                />
+                Add to Favorites
+              </Button>
             </div>
-            <div>
-              <span>Status:</span>
-              <span className="text-white/60">{animeStatus}</span>
-            </div>
-            <div>
-              <span>Aired:</span>
-              <span className="text-white/60">
-                {new Date(
-                  startDate.year,
-                  startDate.month,
-                  startDate.day
-                ).toLocaleDateString()}
-              </span>
-            </div>
-            <div>
-              <span>Season:</span>
-              <span className="text-white/60">{`${season} ${seasonYear}`}</span>
-            </div>
-            <div>
-              <span>Studios:</span>
-              {mainStudios.map((studio) => {
-                return (
-                  <div className="text-white/60" key={studio.node.id}>
-                    {studio.node.name},
-                  </div>
-                );
-              })}
-            </div>
-            <div>
-              <span>Producers:</span>
-              {producers.map((studio) => {
-                return (
-                  <div className="text-white/60" key={studio.node.id}>
-                    {studio.node.name},
-                  </div>
-                );
-              })}
-            </div>
-            <div>
-              <span>Genre:</span>
-              {genres.map((genre, index) => {
-                return (
-                  <div className="text-white/60" key={index}>
-                    {genre},
-                  </div>
-                );
-              })}
-            </div>
-            <div>
-              <span>Romaji:</span>
-              <span className="text-white/60">{title.romaji}</span>
-            </div>
-            <div>
-              <span>English:</span>
-              <span className="text-white/60">{title.english}</span>
-            </div>
-            <div>
-              <span>Native:</span>
-              <span className="text-white/60">{title.native}</span>
-            </div>
-            <div>
-              <span>Synonyms:</span>
-              {synonyms?.map((synonym, index) => {
-                return (
-                  <div className="text-white/60" key={index}>
-                    {synonym},
-                  </div>
-                );
-              })}
+            <div className="space-y-2">
+              {episodes && (
+                <div>
+                  <span className="mr-2">Episodes:</span>
+                  <span className="text-white/60">{episodes}</span>
+                </div>
+              )}
+              {animeStatus && (
+                <div>
+                  <span className="mr-2">Status:</span>
+                  <span className="text-white/60">{animeStatus}</span>
+                </div>
+              )}
+              {startDate && (
+                <div>
+                  <span className="mr-2">Aired:</span>
+                  <span className="text-white/60">
+                    {new Date(
+                      startDate.year,
+                      startDate.month,
+                      startDate.day
+                    ).toLocaleDateString()}
+                  </span>
+                </div>
+              )}
+              {season && seasonYear && (
+                <div>
+                  <span className="mr-2">Season:</span>
+                  <span className="text-white/60">{`${season} ${seasonYear}`}</span>
+                </div>
+              )}
+              {mainStudios && mainStudios.length > 0 && (
+                <div>
+                  <span>Studios:</span>
+                  {mainStudios.map((studio) => {
+                    return (
+                      <div className="text-white/60" key={studio.node.id}>
+                        {studio.node.name}
+                      </div>
+                    );
+                  })}
+                </div>
+              )}
+              {producers && producers.length > 0 && (
+                <div>
+                  <span>Producers:</span>
+                  {producers.map((studio) => {
+                    return (
+                      <div className="text-white/60" key={studio.node.id}>
+                        {studio.node.name}
+                      </div>
+                    );
+                  })}
+                </div>
+              )}
+              {genres && genres.length > 0 && (
+                <div>
+                  <span>Genre:</span>
+                  {genres.map((genre, index) => {
+                    return (
+                      <Link
+                        className="block text-white/60 hover:text-medium-red-violet"
+                        key={index}
+                        href={{
+                          pathname: "/filter",
+                          query: {
+                            genres: [genre],
+                          },
+                        }}
+                      >
+                        {genre}
+                      </Link>
+                    );
+                  })}
+                </div>
+              )}
+              {title.romaji && (
+                <div>
+                  <span className="mr-2">Romaji:</span>
+                  <span className="text-white/60">{title.romaji}</span>
+                </div>
+              )}
+              {title.english && (
+                <div>
+                  <span className="mr-2">English:</span>
+                  <span className="text-white/60">{title.english}</span>
+                </div>
+              )}
+              {title.native && (
+                <div>
+                  <span className="mr-2">Native:</span>
+                  <span className="text-white/60">{title.native}</span>
+                </div>
+              )}
+              {synonyms && synonyms.length > 0 && (
+                <div>
+                  <span>Synonyms:</span>
+                  {synonyms?.map((synonym, index) => {
+                    return (
+                      <div className="text-white/60" key={index}>
+                        {synonym}
+                      </div>
+                    );
+                  })}
+                </div>
+              )}
             </div>
           </div>
           <div className="p-4 grow">
