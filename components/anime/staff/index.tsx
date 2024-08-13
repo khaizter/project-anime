7d916054fbf104b2906db40ef3d2752db358881c
khaizter <vashh.dev@gmail.com>
2024-08-13 23:10:33 +0800

    Anime staffs error handling

--- components/anime/staff/index.tsx
+++ components/anime/staff/index.tsx
@@ -1,8 +1,9 @@
 import { AnimeType } from "@/lib/types";
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import Image from "next/image";
 import { getAnimeStaffs } from "@/lib/anilist";
 import { Skeleton } from "@/components/ui/skeleton";
+import { useToast } from "@/components/ui/use-toast";
 
 interface AnimeCharactersProps {
   anime: AnimeType;
@@ -15,21 +16,39 @@ const AnimeStaff: React.FC<AnimeCharactersProps> = (props) => {
   const { setAnime } = props;
   const { id: animeId, staff: staffs } = props.anime;
   const [isLoading, setIsLoading] = useState(false);
+  const { toast } = useToast();
 
-  useEffect(() => {
-    const fetchStaffs = async () => {
-      setIsLoading(true);
-      const media = await getAnimeStaffs(animeId);
+  const fetchStaffs = useCallback(
+    async (id: number) => {
+      const media = await getAnimeStaffs(id);
       const newStaff = media.staff;
       setAnime((prevState) => {
+        setIsLoading(false);
         return { ...prevState, staff: newStaff };
       });
-      setIsLoading(false);
+    },
+    [setAnime]
+  );
+
+  useEffect(() => {
+    const asyncFunction = async () => {
+      try {
+        if (!staffs) {
+          setIsLoading(true);
+          await fetchStaffs(animeId);
+        }
+      } catch (err) {
+        setIsLoading(false);
+        toast({
+          duration: 3000,
+          variant: "destructive",
+          title: "Something went wrong!",
+          description: "You may also refresh the page or try again later",
+        });
+      }
     };
-    if (!staffs) {
-      fetchStaffs();
-    }
-  }, [animeId, setAnime]);
+    asyncFunction();
+  }, [animeId, staffs, fetchStaffs, toast]);
 
   if (isLoading) {
     return (
@@ -53,26 +72,32 @@ const AnimeStaff: React.FC<AnimeCharactersProps> = (props) => {
   }
 
   return (
-    <ul className="grid grid-cols-1 sm:grid-cols-2 gap-4">
-      {staffs?.edges.map((staff) => {
-        return (
-          <li className="flex justify-between" key={staff.id}>
-            <div className="relative w-16 h-20">
-              <Image
-                src={staff.node.image.medium || ""}
-                alt={`${staff.node.name.full} image`}
-                layout="fill"
-                objectFit="cover"
-              />
-            </div>
-            <div className="text-end">
-              <div>{staff.node.name.full}</div>
-              <div className="text-white/60">{staff.role}</div>
-            </div>
-          </li>
-        );
-      })}
-    </ul>
+    <>
+      {staffs ? (
+        <ul className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+          {staffs?.edges.map((staff) => {
+            return (
+              <li className="flex justify-between" key={staff.id}>
+                <div className="relative w-16 h-20">
+                  <Image
+                    src={staff.node.image.medium || ""}
+                    alt={`${staff.node.name.full} image`}
+                    layout="fill"
+                    objectFit="cover"
+                  />
+                </div>
+                <div className="text-end">
+                  <div>{staff.node.name.full}</div>
+                  <div className="text-white/60">{staff.role}</div>
+                </div>
+              </li>
+            );
+          })}
+        </ul>
+      ) : (
+        <div>Failed to fetch</div>
+      )}
+    </>
   );
 };
 
