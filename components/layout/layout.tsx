8107a354f22f7c780dd72b43606ab2ef932987eb
khaizter <vashh.dev@gmail.com>
2024-08-15 13:23:16 +0800

    Edit title and icon

--- components/layout/layout.tsx
+++ components/layout/layout.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import MainHeader from "@/components/layout/main-header";
-
+import Head from "next/head";
 import { spaceGrotesk, rajdhani, majorMonoDisplay } from "@/lib/fonts";
 import MainFooter from "@/components/layout/main-footer";
 import MobileMenu from "@/components/layout/mobile-menu";
@@ -23,6 +23,9 @@ const Layout = (props: any) => {
   }, []);
   return (
     <div className={`relative ${spaceGrotesk} ${rajdhani} ${majorMonoDisplay}`}>
+      <Head>
+        <title>Project Anime</title>
+      </Head>
       <MainHeader />
       <div className="w-screen overflow-hidden">
         <main className="font-rajdhani text-slate-100 bg-gradient-to-b from-jacaranda to-jacaranda min-h-[80vh]">
